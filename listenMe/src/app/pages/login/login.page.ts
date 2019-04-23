6144a9e5d3b6979b06bccab58db80ddd24c6dbcf
José Glauber <jose.oliveira@ccc.ufcg.edu.br>
2019-04-22 22:32:24 -0300

    alterações login

--- listenMe/src/app/pages/login/login.page.ts
+++ listenMe/src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConsoleReporter } from 'jasmine';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
+
+    username: "";
+    password: "";
+  
   constructor() { }
+
+  login() {
+    console.log("username:" + this.username);
+    console.log("password:" + this.password);
+  }
+
 
   ngOnInit() {
   }
