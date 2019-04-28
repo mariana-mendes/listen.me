f1465e627127caca27dd909993cbfe7a948c0cf6
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2019-04-28 12:59:36 -0300

    Iniciando página de perfil

--- listenMe/src/app/pages/perfil/perfil.page.ts
+++ listenMe/src/app/pages/perfil/perfil.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-perfil',
@@ -10,6 +10,9 @@ export class PerfilPage implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRateChange() {
   }
 
 }
