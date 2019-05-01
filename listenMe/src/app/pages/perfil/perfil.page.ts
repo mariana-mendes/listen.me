ad7065c56d70cb49c3aa2c329dc3ca5bfa6652ce
MatheusHALeal <matheus.leal@ccc.ufcg.edu.br>
2019-05-01 16:46:37 -0300

    Modificando página de perfil

--- listenMe/src/app/pages/perfil/perfil.page.ts
+++ listenMe/src/app/pages/perfil/perfil.page.ts
@@ -1,11 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-
+import { Component, OnInit, ViewChild  } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.page.html',
   styleUrls: ['./perfil.page.scss'],
 })
+
 export class PerfilPage implements OnInit {
+  data: number;
+
+  @ViewChild(IonInfiniteScroll) infiniteScroll: IonInfiniteScroll;
 
   constructor() { }
 
@@ -13,6 +17,26 @@ export class PerfilPage implements OnInit {
   }
 
   onRateChange() {
+  }
+
+  segmentChanged() {
+
+  }
+  loadData(event) {
+    setTimeout(() => {
+      console.log('Done');
+      event.target.complete();
+
+      // App logic to determine if all data is loaded
+      // and disable the infinite scroll
+      if (this.data.length === 1000) {
+        event.target.disabled = true;
+      }
+    }, 500);
+  }
+
+  toggleInfiniteScroll() {
+    this.infiniteScroll.disabled = !this.infiniteScroll.disabled;
   }
 
 }
